07913da3d7bfea5922381c55448d5aaa6e878fb6
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2022-05-28 10:29:07 -0600

    :construction: test on TMP

--- play.py
+++ play.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import re
 import datetime as dt
@@ -5,18 +7,21 @@ import datetime as dt
 # Doing the small stuff first
 # Leave the high level for later
 
-def gen_archiving_name():
+def generate_name():
     NOW = dt.datetime.now()
     iso_date = NOW.strftime("%F") # ISO date
     week_day = NOW.strftime(".%V-%u") # Week 1..53, and Day 1..7 starting on Monday
+    print("generate_name function")
+    print(iso_date + week_day)
     return iso_date + week_day
 
-def get_archive_target():
-
-    RE_EXCLUDE = re.compile(r"^\.|^EM")
+def get_targets():
+    print("get_targets function")
+    RE_EXCLUDE = re.compile(r"^\.|^EM|^em_")
 
     for file in os.listdir():
         if not re.match(RE_EXCLUDE, file):
             print(file)
 
-print(gen_archiving_name())
+generate_name()
+get_targets()
