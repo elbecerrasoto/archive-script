f167bf4db8ec2ae0f63149b1ed57c1eee5c344db
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2022-05-28 11:13:10 -0600

    :recycle: rm prints
    Debugging prints removed.

--- play.py
+++ play.py
@@ -14,12 +14,9 @@ def generate_name():
     NOW = dt.datetime.now()
     iso_date = NOW.strftime("%F") # ISO date
     week_day = NOW.strftime(".%V-%u") # Week 1..53, and Day 1..7 starting on Monday
-    print("generate_name function")
-    print(iso_date + week_day)
     return iso_date + week_day
 
 def get_targets():
-    print("get_targets function")
     RE_EXCLUDE = re.compile(r"archived|^\.|^EM|^em_")
 
     return [i for i in os.listdir() if not re.match(RE_EXCLUDE, i)]
