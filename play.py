bcdb5428c1f5ccbe8ed9f0820ee4cb4cd27dfe42
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2022-05-31 03:27:01 -0600

    :construction: suffix
    date + optional user defined suffix for the name of the archiving directory.

--- play.py
+++ play.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 
-import sys
-import shutil
+import datetime as dt
 import os
 import re
-import datetime as dt
+import shutil
+import sys
 from pathlib import Path
 
 # Doing the small stuff first
 # Leave the high level for later
 
-def generate_name():
+
+def generate_name(suffix: str = ""):
     NOW = dt.datetime.now()
-    iso_date = NOW.strftime("%F") # ISO date
-    week_day = NOW.strftime(".%V-%u") # Week 1..53, and Day 1..7 starting on Monday
-    return iso_date + week_day
+    iso_date = NOW.strftime("%F")  # ISO date
+    week_day = NOW.strftime(".%V-%u")  # Week 1..53, and Day 1..7 starting on Monday
+    return iso_date + week_day + suffix
+
 
 def get_targets():
     RE_EXCLUDE = re.compile(r"archived|^\.|^EM|^em_")
 
     return [i for i in os.listdir() if not re.match(RE_EXCLUDE, i)]
+
 
 def check_create(path):
     path = Path(path)
@@ -28,6 +31,7 @@ def check_create(path):
     else:
         if path.is_file():
             sys.exit(f"Error: {path} is a file, doing NOTHING")
+
 
 if __name__ == "__main__":
 
