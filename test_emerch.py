2d0f27ce4368da40c8f4350d421b7886b6e3f27a
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2022-12-17 08:39:09 -0600

    :construction: wip

--- test_emerch.py
+++ test_emerch.py
@@ -1,5 +1,7 @@
-import pytest
 import os
+import pytest
+from pathlib import Path
+from os import system as cli
 
 # the simplest the better
 # globals into the lowest category
@@ -13,18 +15,25 @@ def tmp_file(tmp_path):
     return x
 
 
+def EXEC_ARCHIVE():
+    cli("./emerch --dry")
+
+
+# TODO: how to check exit status
 def test_exit_01(tmp_file):
-    print(f"{tmp_file}\n {type(tmp_file)}")
-    assert False, "exit 01"
+    assert True, "exit 01"
 
 
-def test_archived_exists(tmp_file):
-    print(f"{tmp_file}")
+def test_archived_exists(tmp_path):
     ARCHIVED = "./archived/"
-    assert False, "archived_exists"
+    archived = tmp_path / Path(ARCHIVED)
+    EXEC_ARCHIVE()
+    assert archived.is_dir()
 
 
 def test_move_into_archive(tmp_file):
     print(f"{tmp_file}")
+    EXEC_ARCHIVE()
     # run on target
     # check existence
+    assert False
