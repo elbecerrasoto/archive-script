a57ce7c81845ba9a70332ee9a676f96e4220a763
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2022-12-20 11:12:27 -0600

    :test_tube: tdd add day dir
    ---
    tdd failing, feature: day dir as a target

--- test_archive.py
+++ test_archive.py
@@ -3,7 +3,13 @@ from pathlib import Path
 import os
 
 
+def gen_naming_scheme():
+    return "today"
+
+
 PROGRAM = Path("./archive").resolve()
+ARCHIVED = Path("archived/")
+DAY = gen_naming_scheme()
 
 
 @pytest.fixture
@@ -19,15 +25,25 @@ def test_exit_with_no_args():
     assert os.system(f"{PROGRAM}") != 0
 
 
-def test_dry_does_nothing(tmp_file, tmp_path):
+def test_dry_does_nothing(tmp_path, tmp_file):
     os.chdir(tmp_path)
     os.system(f"{PROGRAM} --dry {tmp_file}")
-    assert not (tmp_path / "archived/").exists()
+    assert not (tmp_path / ARCHIVED).exists()
 
 
-def test_archived(tmp_file, tmp_path):
+def test_target_is_cleared(tmp_path, tmp_file):
     os.chdir(tmp_path)
     os.system(f"{PROGRAM} {tmp_file}")
-    assert (tmp_path / "archived").exists()
-    assert not tmp_file.exists()
-    assert ((tmp_path / "archived") / tmp_file.name).exists()
+
+    assert not tmp_file.exists(), "! target is cleared"
+
+
+def test_target_arrives_at_correct_destination(tmp_path, tmp_file):
+    os.chdir(tmp_path)
+    os.system(f"{PROGRAM} {tmp_file}")
+
+    destination = tmp_path / DAY / ARCHIVED
+    # assert assert_correct_naming_scheme(day)
+
+    assert destination.exists(), "! destination exists"
+    assert destination / tmp_file.name.exists(), "! correct target destination"
