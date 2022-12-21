4335c0cea698e83f58bb11020ec8a12e839c574d
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2022-12-21 11:46:25 -0600

    :test_tube: name collisions
    ---
    I don't know why I am not catching the exceptions.

--- test_archive.py
+++ test_archive.py
@@ -8,13 +8,13 @@ from utils import gen_naming_scheme
 PROGRAM = Path("./archive").resolve()
 ARCHIVED = Path("archived/")
 DAY = gen_naming_scheme()
+CONTENT = "Destination Demoted!"
 
 
 @pytest.fixture
 def gen_tmp_files(tmp_path):
     PATH = tmp_path
     SUFFIX = ".txt"
-    CONTENT = "Destination Demoted!"
 
     def x(n=1):
 
@@ -61,8 +61,29 @@ def test_target_arrives_at_correct_destination(tmp_path, gen_tmp_files):
     assert (destination / tmp_file.name).exists(), "INcorrect target destination"
 
 
-# def test_name_of_destination_can_be_changed_short_arg():
-#     NAME = "destination_demoted"
+def test_name_collisions(tmp_path, gen_tmp_files):
+    (tmp_file1, tmp_file2) = gen_tmp_files(2)
 
-#     os.chdir(tmp_path)
-#     os.system(f"{PROGRAM} -d {NAME} {tmp_file}")
+    os.chdir(tmp_path)
+
+    destination = tmp_path / ARCHIVED / DAY
+
+    block_dir = destination / tmp_file2.name
+    block_dir.mkdir(parents=True)
+
+    block_file = destination / tmp_file1.name
+    block_file.write_text(CONTENT)
+
+    # Why is not working, cause is not level 1???
+
+    # dir blocking
+    # import shutil
+    # with pytest.raises(shutil.SameFileError):
+    #     os.system(f"{PROGRAM} {tmp_file1}")
+
+    # file blocking
+    # with pytest.raises(FileExistsError):
+    #     os.system(f"{PROGRAM} {tmp_file2}")
+
+    os.system(f"{PROGRAM} {tmp_file1}")
+    os.system(f"{PROGRAM} {tmp_file2}")
